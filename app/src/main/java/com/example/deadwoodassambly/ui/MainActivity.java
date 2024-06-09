34173cef42613d790e638624f8e7173a98b15277
Yatofon <namesurnamecom76@gmail.com>
2024-06-09 15:38:00 +0200

    addFrag

--- app/src/main/java/com/example/deadwoodassambly/ui/MainActivity.java
+++ app/src/main/java/com/example/deadwoodassambly/ui/MainActivity.java
@@ -3,26 +3,24 @@ package com.example.deadwoodassambly.ui;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
+import androidx.navigation.NavGraph;
 import androidx.navigation.Navigation;
 
 import com.example.deadwoodassambly.R;
 import com.example.deadwoodassambly.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
--- app/src/main/java/com/example/deadwoodassambly/ui/assembly/AssemblyFragment.java
+++ app/src/main/java/com/example/deadwoodassambly/ui/assembly/AssemblyFragment.java
@@ -10,48 +10,18 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
-import android.util.Log;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.deadwoodassambly.R;
 import com.example.deadwoodassambly.databinding.FragmentAssemblyBinding;
-import com.example.deadwoodassambly.databinding.FragmentLoggedInBinding;
-import com.example.deadwoodassambly.ui.loginregister.LoggedInViewModel;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.core.Tag;
 
 public class AssemblyFragment extends Fragment {
     private AssemblyViewModel assemblyViewModel;
     private FragmentAssemblyBinding binding;
     private NavController navController;
-//    FirebaseDatabase assemblyDatabase = FirebaseDatabase.getInstance();
-//
-//    DatabaseReference myRef = assemblyDatabase.getReference("message");
-//
-//    public void setMyRef(DatabaseReference myRef) {
-//        myRef.setValue("Hello world!");
-//        myRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                String value = snapshot.getValue(String.class);
-//                String Tag = "tag";
-//                Log.d(Tag, "Value is: " + value);
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                String Tag = "tag";
-//                Log.w(Tag, "Failed to read value.", error.toException());
-//            }
-//        });
-//    }
-
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -62,12 +32,7 @@ public class AssemblyFragment extends Fragment {
         navController= Navigation.findNavController(getActivity(), R.id.nav_host_fragment_activity_main);
         binding = FragmentAssemblyBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        binding.floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                navController.navigate(R.id.navigation_adder);
-            }
-        });
+        binding.floatingActionButton.setOnClickListener(v -> navController.navigate(R.id.navigation_adder));
         return root;
     }
 
